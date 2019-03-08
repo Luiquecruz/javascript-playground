a813c5a2aea3960ce2c46a8d5899ee978cb630b3
Luique Cruz <luiquecruz@gmail.com>
2019-03-08 13:53:01 -0300

    #test2

---
+++ logica-programacao/js4.js
@@ -0,0 +1,11 @@
+var n1 = prompt('digite um número');
+n1 = parseInt(n1);
+
+var n2 = prompt('digite um número');
+n2 = parseInt(n2);
+
+var evenOdd = n1 % n2;
+alert('o resto da visiao é: ' + evenOdd );
+
+var pow = n1 ** n2;
+alert(n1 + ' elevado a ' + n2 + ' é igual á ' + pow);
