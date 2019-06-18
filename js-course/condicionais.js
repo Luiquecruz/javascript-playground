994ce658c119e3943ffd43e56853d0f6b8c5cd90
Luique <luiquecruz@gmail.com>
2019-06-18 07:09:43 -0300

    update

---
+++ js-course/condicionais.js
@@ -0,0 +1,7 @@
+var speed = 87.5
+if (speed > 60) {
+  console.log(`your actual speed is ${speed} km/h, you got CHARGED!`)
+} else {
+  console.log(`your actual speed is ${speed} km/h`)
+  console.log('Be careful!')
+}
